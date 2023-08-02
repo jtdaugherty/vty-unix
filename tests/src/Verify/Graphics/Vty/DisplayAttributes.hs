0c80605794d3a335ae91de7bca4bc965acca07c0
Jonathan Daugherty <cygnus@foobox.com>
2023-08-02 14:42:23 -0700

    Remove unused test library modules

--- tests/src/Verify/Graphics/Vty/DisplayAttributes.hs
+++
@@ -1,8 +0,0 @@
-module Verify.Graphics.Vty.DisplayAttributes
-  ( module Verify.Graphics.Vty.DisplayAttributes
-  , module Graphics.Vty.DisplayAttributes
-  )
-where
-
-import Graphics.Vty.DisplayAttributes
-import Verify
